ab955013412de1dcb7355b8b48162f47b052652e
heydoy <heydoyhey@gmail.com>
2022-07-31 21:25:16 +0900

    배너 클릭시 디테일 화면 - 웹킷뷰 생성

---
+++ Techl/Home/BannerDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BannerDetailViewController.swift
+//  Techl
+//
+//  Created by Doy Kim on 2022/07/31.
+//
+
+import UIKit
+import WebKit
+
+class BannerDetailViewController: UIViewController {
+    static let identifier = "BannerDetailViewController"
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
