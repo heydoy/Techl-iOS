d7c1e432d8250d640f478a38a88d27fbdde61b73
heydoy <heydoyhey@gmail.com>
2022-08-12 19:22:58 +0900

    UserDefaults에 로그인 상태와 jwt 저장할 수  있는 클래스 생성

--- Techl/UserDefaults/LandscapeManager.swift
+++ Techl/UserDefaults/LandscapeManager.swift
@@ -17,4 +17,27 @@ class LandscapeManager {
             UserDefaults.standard.setValue(newValue, forKey: #function)
         }
     }
+ 
+}
+
+class LoginManager {
+    // 로그인 여부 체크
+    static let shared = LoginManager()
+    
+    var isLoggedIn: Bool {
+        get {
+            UserDefaults.standard.bool(forKey: #function)
+            
+        } set {
+            UserDefaults.standard.setValue(newValue, forKey: #function)
+        }
+    }
+    // 로그인 유지상태일 경우 jwt 저장
+    var jwt: String? {
+        get {
+            UserDefaults.standard.string(forKey: "jwt")
+        } set {
+            UserDefaults.standard.setValue(newValue, forKey: "jwt")
+        }
+    }
 }
