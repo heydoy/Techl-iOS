8c0e5935b0cef825132455a75c6557e3dcc24fec
heydoy <heydoyhey@gmail.com>
2022-08-17 19:48:34 +0900

    온보딩 화면의 콘텐츠 텍스트 수정

--- Techl/Sources/Onboarding/OnboardingMessage.swift
+++ Techl/Sources/Onboarding/OnboardingMessage.swift
@@ -15,9 +15,9 @@ struct OnboardingMessage {
 
 extension OnboardingMessage {
     static let messages: [OnboardingMessage]  = [
-        OnboardingMessage(imageName: "onboarding-1", title: "Discuss", description: "기술서적의 후기와 담론을\n포럼에서 자유롭게 할 수 있어요"),
-        OnboardingMessage(imageName: "onboarding-2", title: "Read", description: "북클럽/챌린지에서\n기술서적을 함께 읽어봐요."),
-        OnboardingMessage(imageName: "onboarding-3", title: "Bookmark", description: "관심있는 서적들을 북마크하여\n새 활동을 모아볼 수 있어요.")
+        OnboardingMessage(imageName: "onboarding-1", title: "DISCUSS", description: "기술서적의 후기와 담론을\n포럼에서 자유롭게 할 수 있어요"),
+        OnboardingMessage(imageName: "onboarding-2", title: "READ TOGETHER", description: "북클럽/챌린지에서\n기술서적을 함께 읽어봐요."),
+        OnboardingMessage(imageName: "onboarding-3", title: "BOOKMARK", description: "관심있는 서적들을 북마크하여\n새 활동을 모아볼 수 있어요.")
     
     ]
 }
