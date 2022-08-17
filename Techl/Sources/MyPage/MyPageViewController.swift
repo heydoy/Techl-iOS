39f21b69c80b74a030677b358a3ab6dc9fe82116
heydoy <heydoyhey@gmail.com>
2022-08-17 19:48:00 +0900

    마이페이지 화면디자인 메서드 선언

--- Techl/Sources/MyPage/MyPageViewController.swift
+++ Techl/Sources/MyPage/MyPageViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 class MyPageViewController: UIViewController {
     static let identifier = "MyPageViewController"
     
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var forumActivityBaseView: UIView!
+    @IBOutlet weak var clubActivityBaseView: UIView!
+    @IBOutlet weak var bookmarkActivityBaseView: UIView!
+    
+    var viewList: [ UIView ] { [ forumActivityBaseView, clubActivityBaseView, bookmarkActivityBaseView
+        ]
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigation()
-    
+        configureUI()
     }
     
     @IBAction func nicknameChangeButtonTapped(_ sender: UIButton) {
         print("닉네임 변경")
+    }
+    
+    func configureUI() {
+        
+        profileImageView.clipsToBounds = true
+        profileImageView.layer.cornerRadius = profileImageView.frame.size.width / 2
+        
+        viewList.forEach {
+            $0.clipsToBounds = true
+            $0.layer.cornerRadius = 12
+            $0.layer.borderColor = UIColor.systemGray6.cgColor
+            $0.layer.borderWidth = 1
+        }
     }
     
     
