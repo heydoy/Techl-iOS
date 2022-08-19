27dc7a91ea68be71ab138ee55b6e0b543cc08f17
heydoy <heydoyhey@gmail.com>
2022-08-20 00:44:06 +0900

    앱 사용 시 처음 접속, 또는 로그인 안했을 시 온보딩 페이지 / 자동로그인 시 메인으로 이동하도록 하는 컨시어지 기능 구현, 검증

--- Techl/Sources/Main/ConciergeViewController.swift
+++ Techl/Sources/Main/ConciergeViewController.swift
@@ -19,17 +19,21 @@ class ConciergeViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        if LandscapeManager.shared.isFirstLaunch {
+        if LandscapeManager.shared.isFirstLaunch == false {
             performSegue(withIdentifier: "toOnboarding", sender: nil)
             
             LandscapeManager.shared.isFirstLaunch = true
             
         } else {
-            
-            performSegue(withIdentifier: "toOnboarding", sender: nil)
-            LandscapeManager.shared.isFirstLaunch = true
-            
-            performSegue(withIdentifier: "toAuthentication", sender: nil)
+            if LoginManager.shared.jwt == nil {
+                performSegue(withIdentifier: "toOnboarding", sender: nil)
+                //performSegue(withIdentifier: "toAuthentication", sender: nil)
+                
+            } else {
+                // 로그인 되어있는 상태면 바로 메인으로 이동
+                // 기존 뷰 날리는 작업이 필요함
+                performSegue(withIdentifier: "toMain", sender: nil)
+            }
         }
     }
 
