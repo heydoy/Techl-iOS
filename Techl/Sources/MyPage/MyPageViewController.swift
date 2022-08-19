c175c74b8ea1a53847562c66c92b9f1a9a1c7603
heydoy <heydoyhey@gmail.com>
2022-08-20 00:44:48 +0900

    마이페이지에 로그아웃기능 구현

--- Techl/Sources/MyPage/MyPageViewController.swift
+++ Techl/Sources/MyPage/MyPageViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Toast
 
 class MyPageViewController: UIViewController {
     static let identifier = "MyPageViewController"
@@ -59,7 +60,43 @@ class MyPageViewController: UIViewController {
     }
     
     
-
+    @IBAction func logoutButtonTapped(_ sender: UIButton) {
+        
+        // 로그아웃 알림
+        let alert = UIAlertController(title: "로그아웃 하시겠습니까?", message: nil, preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "확인", style: .destructive) { action in
+            self.view.makeToast("로그인 화면으로 돌아갑니다.", duration: 0.2, position: .center, title: nil, image: nil, style: ToastStyle()) { didTap in
+                self.backToLogin()
+            }
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        self.present(alert, animated: true)
+        
+        
+        
+    }
+    
+    func backToLogin() {
+        // 확인 눌렀을 시에만 로그아웃하도록 사용
+        
+        let storyboard = UIStoryboard(name: "Authentication", bundle: nil)
+        
+        let vc = storyboard.instantiateViewController(withIdentifier: ConciergeLoginViewController.identifier ) as! ConciergeLoginViewController
+        
+        vc.modalPresentationStyle = .fullScreen
+        vc.modalTransitionStyle = .crossDissolve
+        
+        // 기존 뷰를 다 날리는 작업이 필요함
+        
+        self.present(vc, animated: true)
+    }
+    
     
 
 }
