2bdf1a9f9104fe0d88ef112735f4e4688468be67
heydoy <heydoyhey@gmail.com>
2022-08-26 09:48:11 +0900

    포럼 게시글 작성에 이미지피커

--- Techl/Sources/Forum/ForumEditorViewController.swift
+++ Techl/Sources/Forum/ForumEditorViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Toast
+import YPImagePicker
 
 class ForumEditorViewController: UIViewController {
     
@@ -17,6 +18,8 @@ class ForumEditorViewController: UIViewController {
     
     @IBOutlet weak var cameraButton: UIButton!
     
+    @IBOutlet weak var imageView: UIImageView!
+    
     
     var userText: String = ""
     
@@ -26,18 +29,31 @@ class ForumEditorViewController: UIViewController {
         
         userTextView.textContainerInset = UIEdgeInsets(top: 20, left: 24, bottom: 20, right: 24)
         userTextView.delegate = self
-
+        
     }
     
     
     @IBAction func cameraButtonTapped(_ sender: UIButton) {
-        
-        // 카메라 없앨 수도 있음
+        let picker = YPImagePicker()
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let photo = items.singlePhoto {
+                print(photo.fromCamera) // Image source (camera or library)
+                print(photo.image) // Final image selected by the user
+                print(photo.originalImage) // original image selected by the user, unfiltered
+                print(photo.modifiedImage) // Transformed image, can be nil
+                print(photo.exifMeta) // Print exif meta data of original image.
+                
+                
+                self.imageView.image = photo.image
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+        present(picker, animated: true, completion: nil)
     }
     
     
     
-
+    
     
     
     @IBAction func finishButtonTapped(_ sender: UIButton) {
@@ -46,33 +62,44 @@ class ForumEditorViewController: UIViewController {
         var style = ToastStyle()
         style.messageColor = .systemGray6
         
-        APIManager.shared.forumNewPostRequest(title: userTextView.text , content: userTextView.text) {
-            print("---> 글 작성 중 ")
+        //guard let imageData = imageView.image?.jpegData(compressionQuality: 0.3) else { return }
+        
+        guard let imageData = imageView.image else { return }
+        
+        APIManager.shared.forumNewPostWithImageRequest(title: userTextView.text , content: userTextView.text , image: imageData) {
             self.view.makeToast("글쓰기가 완료되었습니다.", duration: 0.7, position: .bottom, title: nil, image: nil, style: style) { didTap in
                 self.dismiss(animated: true)
             }
         }
+            
+            //        // 기존
+            //        APIManager.shared.forumNewPostRequest(title: userTextView.text , content: userTextView.text) {
+            //
+            //            self.view.makeToast("글쓰기가 완료되었습니다.", duration: 0.7, position: .bottom, title: nil, image: nil, style: style) { didTap in
+            //                self.dismiss(animated: true)
+            //            }
+            //        }
+            
+            
+            
+            
+        }
         
         
-        
+        @IBAction func closeButtonTapped(_ sender: UIButton) {
+            // 닫기 버튼 눌렀을 때
+            
+            self.dismiss(animated: true)
+            
+            
+        }
         
     }
     
-    
-    @IBAction func closeButtonTapped(_ sender: UIButton) {
-        // 닫기 버튼 눌렀을 때
+    extension ForumEditorViewController: UITextViewDelegate {
         
-        self.dismiss(animated: true)
-        
+        func textViewDidChange(_ textView: UITextView) {
+            self.userText = textView.text
+        }
         
     }
-    
-}
-
-extension ForumEditorViewController: UITextViewDelegate {
-    
-    func textViewDidChange(_ textView: UITextView) {
-        self.userText = textView.text
-    }
-    
-}
