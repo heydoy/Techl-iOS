d37fd3f9cef8b336c557ee93045bfe15b3e56d99
heydoy <heydoyhey@gmail.com>
2022-08-26 09:50:19 +0900

    북클럽 콜렉션 뷰 셀에, 데이터를 받아와 UI에 적용시키는 메서드 추가

--- Techl/Sources/View/ClubCollectionViewCell.swift
+++ Techl/Sources/View/ClubCollectionViewCell.swift
@@ -12,5 +12,14 @@ class ClubCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var clubCardView: ClubCardView!
 
+    func configureData(club: ClubModel) {
+        clubCardView.clubImageView.image = UIImage(named: club.thumbnail)
+        clubCardView.badgeLabel.text = club.badge
+        clubCardView.statusLabel.text = club.status
+        clubCardView.titleLabel.text = club.title
+        clubCardView.locationLabel.text = club.location
+        clubCardView.descriptionLabel.text = club.description
+        clubCardView.startDateLabel.text = club.startDate
+    }
     
 }
